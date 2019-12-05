9e93326cada6ba988372446c31ee08ec2df3f971
Otto Cedeno <otto@ottostacos.com>
2019-12-05 03:05:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -48,7 +48,7 @@ class Song
 
   def self.new_from_filename(filename)
     song_data = filename.split(' - ')
-    s = Song.new
+    s = self.new
     s.artist_name = song_data[0]
     s.name = song_data[1].chomp(".mp3")
     s
