efbc1f1f8a080653a15cea62da7d1f217027f1ef
Otto Cedeno <otto@ottostacos.com>
2019-12-05 01:48:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -36,8 +36,7 @@ class Song
   end
 
   def self.find_or_create_by_name(song_name)
-    song = self.find_by_name(song_name)
-    song ||= self.create_by_name(song_name)
+  s elf.find_by_name(song_name) ||= self.create_by_name(song_name)
   end
 end
 
